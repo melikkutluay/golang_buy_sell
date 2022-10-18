9d96e7d6e277878041fed6694ca4f6f131b21b07
melikktly <melik.kutluay@softtech.com.tr>
2022-10-18 08:52:22 +0300

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # golang_buy_sell
+# Documentation
+ 
+https://documenter.getpostman.com/view/14336225/2s847EQtxj
--- controller.go
+++ controller.go
@@ -17,6 +17,15 @@ type loginRequest struct {
 	Password string `json:"password"`
 }
 
+type buyRequest struct {
+	Kaynak_id    int    `json:"Kaynak_id"`
+	Hedef_id     int    `json:"Hedef_id"`
+	Kaynak_hesap string `json:"Kaynak_hesap"`
+	Hedef_hesap  string `json:"Hedef_hesap"`
+	Miktar       int    `json:"Miktar"`
+	User_id      string `json:"User_id"`
+}
+
 type loginResponse struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
@@ -24,9 +33,24 @@ type loginResponse struct {
 }
 
 type Exchange struct {
-	exchange_id   int    `json:"exchange_id"`
-	exchange_name string `json:"exchange_name"`
-	exchange_rate int    `json:"exchange_rate"`
+	Exchange_id   int    `json:"exchange_id"`
+	Exchange_name string `json:"exchange_name"`
+	Exchange_rate int    `json:"exchange_rate"`
+}
+
+type Account struct {
+	//Account_id   int    `json:"account_id"`
+	//Account_name string `json:"account_name"`
+	Balance int `json:"balance"`
+	//User_id      int    `json:"User_id"`
+}
+
+type Buy struct {
+	kaynak_id    int    `json:"kaynak_id"`
+	hedef_id     int    `json:"kaynak_id"`
+	kaynak_hesap string `json:"kaynak_hesap"`
+	hedef_hesap  string `json:"kaynak_hesap"`
+	miktar       int    `json:"exchange_rate"`
 }
 
 type controller struct {
@@ -73,17 +97,37 @@ func (c *controller) login(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *controller) makeBuy(w http.ResponseWriter, r *http.Request) {
+	rq := &buyRequest{}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(rq); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("server: could not read request body: %s\n", err)
+	}
+
+	fmt.Printf("server: request body: %s\n", reqBody)
+	ss := fmt.Sprintf("select balance from accounts where user_id = '%s' and account_id = %d", rq.User_id, rq.Kaynak_id)
+	fmt.Println(ss)
+	getMoney := getQeury(ss)
+	fmt.Println(getMoney)
+	rows := makingBuy(ss)
+	var AccountList []Account
+	for rows.Next() {
+		var account Account
+		if err := rows.Scan(&account.Balance); err != nil {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		AccountList = append(AccountList, account)
+	}
+	rsJs, _ := json.Marshal(AccountList)
 	w.Header().Set("Content-Type", "application/json")
-	payload := Exchange{}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(payload.exchange_name)
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(rsJs)
+
 }
 
 func (c *controller) makeSell(w http.ResponseWriter, r *http.Request) {
@@ -92,10 +136,10 @@ func (c *controller) makeSell(w http.ResponseWriter, r *http.Request) {
 
 func getExchangeRate() {
 	var exc Exchange
-	for range time.Tick(time.Second * 1) {
+	for range time.Tick(time.Second * 10) {
 		rows := getQeury("SELECT * FROM exchange")
 		for rows.Next() {
-			if err := rows.Scan(&exc.exchange_id, &exc.exchange_name, &exc.exchange_rate); err != nil {
+			if err := rows.Scan(&exc.Exchange_id, &exc.Exchange_name, &exc.Exchange_rate); err != nil {
 				log.Fatal(err)
 			}
 			fmt.Println(exc)
